ad4eb1237751e00504999ddd944db07a3cead2d4
asstrallity-ui <asstrallity@gmail.com>
2025-11-21 07:36:02 +0300

    Add files via upload

---
+++ Data/S.js
@@ -0,0 +1,26 @@
+const MODS_DATABASE = [
+    {
+        // Это для кода "VIP-1" из вашего Gist
+        code: "ASSTRALLITY", 
+        name: "ASSTRALLITY PACK",
+        author: "ASSTRALLITY",
+        description: "Приватный пак текстур высокого разрешения",
+        version: "2.1.3",
+        file: "2.zip",
+        type: "zip",
+        password: "123",
+        size: "156.8 MB"
+    },
+    {
+        // Это для кода "TEST_CODE" из вашего Gist
+        code: "TEST_MOD",
+        name: "Test Modification",
+        author: "Dev Team",
+        description: "Тестовый файл для проверки системы",
+        version: "1.0",
+        file: "test.asstrallity",
+        type: "asstrallity",
+        password: null,
+        size: "2.5 MB"
+    }
+];
